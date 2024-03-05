940ea2afa473befd311fcceb4abb7cb4796e1d3f
st178242 <st178242@stud.uni-stuttgart.de>
2024-03-05 16:53:36 +0100

    Inital KeyWord extraction

---
+++ src/keyword_extraction.py
@@ -0,0 +1,31 @@
+﻿import csv
+
+BloomstaxonomieDE = [[], [], [], [], [], []]
+BloomstaxonomieEN = [[], [], [], [], [], []]
+
+
+def Initialize():
+    with open('bloom_keywords_en_de.csv', 'r', encoding='utf-8') as csv_datei:
+        csv_reader = csv.reader(csv_datei, delimiter=';')
+        next(csv_reader)
+        rowNumber = 0
+        for row in csv_reader:
+            if rowNumber == 0:
+                for i in range(0, 6):
+                    BloomstaxonomieDE[i].extend([element.strip() for element in row[i].split(',')])
+            if rowNumber == 1:
+                for i in range(0, 6):
+                    BloomstaxonomieEN[i].extend([element.strip() for element in row[i].split(',')])
+            rowNumber += 1
+
+
+
+def IsInBloom(KeyWord):
+    Initialize()
+    KeyWord = KeyWord.capitalize()
+    print(KeyWord)
+    for i in range(0, 6):
+        print(BloomstaxonomieEN[i])
+        if KeyWord in BloomstaxonomieDE[i] or KeyWord in BloomstaxonomieEN[i]:
+            return i
+    return 'N'
