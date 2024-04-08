9b79b3449dd787c638531257935a596e444c5f05
maxeljaxel <64484632+maxeljaxel@users.noreply.github.com>
2024-04-08 12:53:45 +0200

    Merge Keywordextraction (#2)
    * Added Diagram to show how our data should be build
    
    * Added a picture of our data sets
    
    * Added comments for better understanding

---
+++ src/tag_questions.py
@@ -0,0 +1,32 @@
+import json
+import KeyWordExtraction as kwe
+
+def read_json_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print("Following Error apeard", e)
+
+#Takes a json file (probably just 1 explicit)
+#Creats a list of tuples the tuples contain a tag(id) pattern(the question), response(answer to the question), a bloom Tag(the bloom category)
+def create_tuple_list(data):
+    tuple_list = []
+    if data:
+        intents = data['intents']
+        for intent in intents:
+            tag = intent['tag']
+            patterns = intent['patterns']
+            responses = intent['responses']
+            bloom_tag = kwe.get_bloom_tag(patterns)
+            for pattern, response in zip(patterns, responses):
+                tuple_list.append((tag, pattern, response))
+    return tuple_list
+
+# Beispielaufruf
+filename = 'beispiel.json'  # Passe den Dateinamen entsprechend an
+data = read_json_file(filename)
+
