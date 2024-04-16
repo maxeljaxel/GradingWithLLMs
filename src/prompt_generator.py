549d0b6597a5b79a1e939b9b57d0db9787994e42
David Enoghama <david.enoghama@outlook.de>
2024-04-16 13:25:54 +0200

    Changed the prompt_generator.py . It generates now prompts with steps it should follow during the assessment. The steps are now also customized on the information, which is delivered to the method.

--- src/prompt_generator.py
+++ src/prompt_generator.py
@@ -7,14 +7,14 @@ def bloom_prompt_generator(self, bloom_level: object) -> str:
     # Knowledge
     if bloom_level == 0:
         prompt = """\nThe bloom level is knowledge. As an assistant you will need to check, if the student can recall 
-        relevant facts and basic concepts involving the questions topic area."""
+relevant facts and basic concepts involving the questions topic area."""
     # Comprehension
     elif bloom_level == 1:
         prompt = """\nThe bloom level is comprehension. As an assistant you will need to check, if the student is able 
-        to explain the specified idea or concept well."""
+to explain the specified idea or concept well."""
     elif bloom_level == 2:
         prompt = """\nThe bloom level is application. The student have to apply a concept and you as an assistant 
-        have to check, if the student uses the right concept and if the answer of resulting of the usage is correct."""
+have to check, if the student uses the right concept and if the answer of resulting of the usage is correct."""
     # Need to be prompt engineered later. We make a standardized prompt for the upper three levels: Analysis, Synthesis
     # and Evaluation
     #  elif bloom_level == 3:
@@ -35,7 +35,7 @@ an appropriate manner of the bloom level."""
 # for that involves keywords.
 def keyword_prompt_generator(self, keyword):
     prompt = ""
-    if len(keyword) != 0:
+    if self.keyword_present:
         words = ""
         for word in keyword:
             words += word
@@ -52,33 +52,51 @@ this would benefit the feedback.
 # This method creates a substring of the prompt for the GPT-4 API. If a solution is delivered the prompt will include it
 # otherwise it orders GPT-4 to generate its own solution.
 def example_prompt_generator(self, example_solution):
-    if example_solution is not None:
+    if self.solution_present:
         prompt = f"\nThe professor delivered the following example solution:\n{example_solution}"
     else:
         prompt = """There is no example solution delivered by the professor. Generate multiple solution to the 
-        corresponding to the possible achievable points for the question."""
+corresponding to the possible achievable points for the question."""
     return prompt
 
-
+# This method generates the final prompt. It uses the specific information of the whole task and generates a prompt
+# based on the information
 def final_prompt_generator(self):
+    step_counter = 0
+    prompt_appendix_steps = ""
+    if self.question[2] == "N":
+        step_counter += 1
+        prompt_appendix_steps += f"\n{step_counter}. Determine the taxonomy level"
+    if self.keyword_present:
+        step_counter += 1
+        prompt_appendix_steps += f"\n{step_counter}. Check if the keywords are contextual mentioned in the answer"
+    if self.solution_present:
+        step_counter += 1
+        prompt_appendix_steps += f"\n{step_counter}. Compare the professor solution with the answer of the student"
+    else:
+        step_counter += 1
+        prompt_appendix_steps += f"\n{step_counter}. Compare your solution with the answer of the student"
+    step_counter += 1
+    prompt_appendix_steps += f"""\n{step_counter}. Evaluate if the answer of the student is right or wrong and if there are missing or false information please 
+    highlight them and explain, why these information are false."""
+
     prompt = f"""You are an AI assistant that helps with the assessment of free text answers in the subject software 
 engineering and programming. You will receive the question and the student answer to this question.\n{self.bloom_prompt}
 \nThe examination question is: \n{self.question[0]}{self.keyword_prompt}{self.example_solution_prompt}
     \nWith all this information to will now evaluate the incoming student answer.
     \nDo the following steps:
-    \n1. Determine if not already done the taxonomy level
-    \n2. Check if the keywords are contextual mentioned in the answer
-    \n3. Compare your solution or if giving the professor solution with the answer of the student
-    \n4. Evaluate if the answer of the student is right or wrong and if there are missing or false information please 
-    highlight them and explain, why these information are false.
     """
-    return prompt
+    return prompt + prompt_appendix_steps
 
 
 # Basic prompt generator class. Purpose is to dynamically modify the content for the role system/assistant according to
 # the question and several settings, that are included.
 class PromptGenerator:
     question = {}
+    keyword_present = False
+    solution_present = False
+
+
 
     def __init__(self):
         self.bloom_prompt = ""
@@ -88,10 +106,12 @@ class PromptGenerator:
 
     # End-point for the prompt generation. Needs the question tuple, the keyword list and the exampl solution. None of
     # the last two attributes need a value for this method to work perfectly.
-    def generate_prompts(self, question, keyword, example_solution):
+    def generate_prompts(self, question, keywords, example_solution):
+        self.keyword_present = len(keywords) > 0
+        self.solution_present = example_solution is not None
         self.question = question
         self.bloom_prompt = bloom_prompt_generator(self, question[2])
-        self.keyword_prompt = keyword_prompt_generator(self, keyword)
+        self.keyword_prompt = keyword_prompt_generator(self, keywords)
         self.example_solution_prompt = example_prompt_generator(self, example_solution)
         final_prompt = final_prompt_generator(self)
         return final_prompt
